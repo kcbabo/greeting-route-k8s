a849ed9f98f71b6a5a4f23dd0e03661f57569b7b
Keith Babo <kbabo@redhat.com>
2016-01-18 22:48:40 -0500

    updates

--- src/main/java/org/example/GreeterBean.java
+++ src/main/java/org/example/GreeterBean.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.camel.Exchange;
+
 public class GreeterBean {
 	
 	private Random random = new Random();
@@ -11,18 +13,34 @@ public class GreeterBean {
 	
 	public GreeterBean() {
 		names.add("Keith");
-		names.add("Diogenes");
+		names.add("rettori");
 		names.add("Bill");
 		names.add("Steve");
 		names.add("George");
 		names.add("Fabio");
 	}
-
-	public String greet(String name) {
-		return "Howdy, " + name + "! Howya doin', buddy?";
+	
+	public String postProcess(String msg) {
+		msg = msg.replaceAll("&lt;", "<");
+		msg = msg.replaceAll("&gt;", ">");
+		msg = msg.replaceAll("&quot;", "\"");
+		return msg;
 	}
 	
 	public String generateName() {
-		return names.get(random.nextInt(5));
+		String name = names.get(random.nextInt(5));
+		System.out.println("Using name: " + name);
+		return REQUEST.replaceAll("\\$NAME", name);
 	}
+	
+	private static final String REQUEST = 
+			"<batch-execution lookup=\"HelloRulesSession\">"
+			+ "<insert>"
+			+   "<org.openshift.quickstarts.decisionserver.hellorules.Person>"
+			+   "<name>$NAME</name>"
+			+   "</org.openshift.quickstarts.decisionserver.hellorules.Person>"
+			+ "</insert>"
+			+ "<fire-all-rules/>"
+			+ "<query out-identifier=\"greetings\" name=\"get greeting\"/>"
+			+ "</batch-execution>";
 }
